b8abec2f60750688827cfd87198992ec720a1a7a
claupanca <55442866+claupanca@users.noreply.github.com>
2024-08-02 17:53:29 +0300

    13. Perfoemance Optimization - 25%

--- 00. Projects/11_atomic_blog/src/App-v1.jsx
+++ 00. Projects/11_atomic_blog/src/App-v1.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostProvider";
+import { memo } from "react";
 
 function createRandomPost() {
   return {
@@ -201,8 +202,8 @@ function List() {
   );
 }
 
-function Archive() {
-  const { onAddPost } = usePosts();
+const Archive = memo(function Archive() {
+  // const { onAddPost } = usePosts();
 
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
@@ -226,14 +227,14 @@ function Archive() {
               <p>
                 <strong>{post.title}:</strong> {post.body}
               </p>
-              <button onClick={() => onAddPost(post)}>Add as new post</button>
+              {/* <button onClick={() => onAddPost(post)}>Add as new post</button> */}
             </li>
           ))}
         </ul>
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
