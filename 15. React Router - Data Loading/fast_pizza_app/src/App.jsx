9f380b722be3df43abda70af73150f71dc576e37
claupanca <55442866+claupanca@users.noreply.github.com>
2024-08-12 13:49:48 +0300

    15. React Router - Data Loading - 15%

--- 15. React Router - Data Loading/fast_pizza_app/src/App.jsx
+++ 15. React Router - Data Loading/fast_pizza_app/src/App.jsx
@@ -1,5 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import Menu from "./features/menu/Menu";
+// import Home from "./ui/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    // we define params using :orderId
+    path: "/order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <h1>Hello</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
