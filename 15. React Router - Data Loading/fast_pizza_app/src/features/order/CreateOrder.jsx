d74a785f7389b83e6608ae6661b07423e7d95c11
claupanca <55442866+claupanca@users.noreply.github.com>
2024-08-13 11:22:03 +0300

    15. React Router - Data Loading - 100%

--- 15. React Router - Data Loading/fast_pizza_app/src/features/order/CreateOrder.jsx
+++ 15. React Router - Data Loading/fast_pizza_app/src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
-import { Form } from "react-router-dom";
+import { Form, redirect, useNavigation, useActionData } from "react-router-dom";
 
 import { useState } from "react";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -36,6 +37,14 @@ export default function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
 
+  // we use this to change the button while the form is sending data
+  const navigation = useNavigation();
+  console.log("navigation", navigation);
+
+  // we use this to get access to the Errors Object
+  const formErrors = useActionData();
+  console.log("formErrors", formErrors);
+
   return (
     <div>
       <h2>Ready to order? Let's go!</h2>
@@ -53,6 +62,7 @@ export default function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -74,7 +84,10 @@ export default function CreateOrder() {
         </div>
 
         <div>
-          <button>Order now</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button disabled={navigation.state === "submitting" ? true : false}>
+            Order now
+          </button>
         </div>
       </Form>
     </div>
@@ -82,10 +95,33 @@ export default function CreateOrder() {
 }
 
 export async function orderAction(request) {
-  // const formData =
-  // const data = Object.fromEntries(await request.formData());
-  // console.log("formData", formData);
-  console.log("request", request);
-  // console.log("data", data);
-  return null;
+  const formData = await request.request.formData();
+  const data = Object.fromEntries(formData);
+  console.log("formData", formData);
+  console.log("data", JSON.stringify(data));
+
+  // createOrder(data);
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
+
+  console.log("order", order);
+
+  // FORM VALIDATION and Error Handling
+  const errors = {};
+  if (!isValidPhone(order.phone)) errors.phone = "Phone number is incorrect";
+
+  // if the ERRORS object has some data, we return the errors instead of the
+  // newly created Order
+  if (Object.keys(errors).length > 0) return errors;
+
+  // if there are no ERRORS, the order is sent to the API
+  // newOrder is coming back as a response from the API
+  const newOrder = await createOrder(order);
+  console.log("newOrder is submitted", newOrder);
+
+  // and we are redirected to the Order Page
+  return redirect(`/order/${newOrder.id}`);
 }
