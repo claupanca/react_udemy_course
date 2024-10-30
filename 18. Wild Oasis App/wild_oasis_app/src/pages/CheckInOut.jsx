18da87a82dae17d70c06243f27bef44997fef60b
claupanca <55442866+claupanca@users.noreply.github.com>
2024-10-30 17:31:44 +0200

    22. Extra Features - Checkin a booking - 50%

---
+++ 18. Wild Oasis App/wild_oasis_app/src/pages/CheckInOut.jsx
@@ -0,0 +1,13 @@
+import Row from "../ui/Row";
+
+import CheckinBooking from "../features/check-in-out/CheckinBooking";
+
+function CheckInOut() {
+  return (
+    <Row type="vertical">
+      <CheckinBooking />
+    </Row>
+  );
+}
+
+export default CheckInOut;
